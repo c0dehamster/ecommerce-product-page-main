ca4ca606a07118147c512cc8fa82a4a12a9448d4
Sergej Smelov <sergeysmelow@gmail.com>
2023-04-14 22:48:29 +0500

    Write basic markdown of the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,30 @@
 <script>
 	import "./style.css"
+
+	import Navigation from "./Navigation.svelte"
+	import Cart from "./Cart.svelte"
+
+	import logo from "../lib/images/logo.svg"
+	import avatar from "../lib/images/image-avatar.png"
 </script>
 
-<slot />
+<div class="app">
+	<header class="header">
+		<div class="header__align-left">
+			<img src={logo} alt="sneakers" class="logo" />
+			<Navigation />
+		</div>
+
+		<div class="header__align-right">
+			<Cart />
+
+			<button class="button button--profile" aria-label="My profile">
+				<img src={avatar} alt="" class="avatar" />
+			</button>
+		</div>
+	</header>
+
+	<main class="wrapper">
+		<slot />
+	</main>
+</div>
