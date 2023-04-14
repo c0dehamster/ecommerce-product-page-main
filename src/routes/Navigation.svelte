60282271f3ed6c5bdf4cdb15dc5b1bb4d9fb5ba9
Sergej Smelov <sergeysmelow@gmail.com>
2023-04-14 23:40:08 +0500

    Add markdown for the nav (desktop)

---
+++ src/routes/Navigation.svelte
@@ -0,0 +1,23 @@
+<nav class="nav">
+	<ul class="nav__list">
+		<li class="nav__list-item">
+			<a href="#" class="nav__link">Collections</a>
+		</li>
+
+		<li class="nav__list-item">
+			<a href="#" class="nav__link">Men</a>
+		</li>
+
+		<li class="nav__list-item">
+			<a href="#" class="nav__link">Women</a>
+		</li>
+
+		<li class="nav__list-item">
+			<a href="#" class="nav__link">About</a>
+		</li>
+
+		<li class="nav__list-item">
+			<a href="#" class="nav__link">Contact</a>
+		</li>
+	</ul>
+</nav>
