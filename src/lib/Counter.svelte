8bf4318b2f2edb39536bb87b3623bfdeb50cc9df
Sergej Smelov <sergeysmelow@gmail.com>
2023-04-14 23:20:11 +0500

    Markdown of the counter

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,16 @@
+<script>
+	import iconMinus from "./images/icon-minus.svg"
+	import iconPlus from "./images/icon-plus.svg"
+</script>
+
+<div class="counter">
+	<button class="counter__button">
+		<img src={iconMinus} alt="remove" class="counter__icon" />
+	</button>
+
+	<p class="value">0</p>
+
+	<button class="counter__button">
+		<img src={iconPlus} alt="add" class="counter__icon" />
+	</button>
+</div>
