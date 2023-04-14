55645d97a94be2b997b91b5cca93eb3a2a5c5afe
Sergej Smelov <sergeysmelow@gmail.com>
2023-04-14 23:05:51 +0500

    Write markdown of the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Gallery from "../lib/Gallery.svelte"
+	import Counter from "../lib/Counter.svelte"
+
+	import iconCart from "../lib/images/icon-cart.svg"
+</script>
+
+<svelte:head>
+	<title>Frontend Mentor | E-commerce product page</title>
+</svelte:head>
+
+<div class="gallery">
+	<Gallery />
+</div>
+
+<section class="product-card">
+	<p class="manufacturer">Sneaker Company</p>
+	<h1 class="heading">Fall Limited Edition Sneakers</h1>
+	<p class="description">
+		These low-profile sneakers are your perfect casual wear companion.
+		Featuring a durable rubber outer sole, they’ll withstand everything the
+		weather can offer.
+	</p>
+
+	<div class="price">
+		<div class="price__relevant">
+			<p class="price__new">$125.00</p>
+			<p class="price__discount">50%</p>
+		</div>
+
+		<p class="price__old"><s>$250.00</s></p>
+	</div>
+
+	<Counter />
+
+	<button class="button">
+		<img src={iconCart} alt="" class="icon" />
+		<span class="button__text">Add to cart</span>
+	</button>
+</section>
