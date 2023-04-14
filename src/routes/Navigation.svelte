82088ef290381b45fa97da597eec4c29992b771f
Sergej Smelov <sergeysmelow@gmail.com>
2023-04-14 23:51:47 +0500

    Change href of mock-up links to a safer one

--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -1,23 +1,23 @@
 <nav class="nav">
 	<ul class="nav__list">
 		<li class="nav__list-item">
-			<a href="#" class="nav__link">Collections</a>
+			<a href="#0" class="nav__link">Collections</a>
 		</li>
 
 		<li class="nav__list-item">
-			<a href="#" class="nav__link">Men</a>
+			<a href="#0" class="nav__link">Men</a>
 		</li>
 
 		<li class="nav__list-item">
-			<a href="#" class="nav__link">Women</a>
+			<a href="#0" class="nav__link">Women</a>
 		</li>
 
 		<li class="nav__list-item">
-			<a href="#" class="nav__link">About</a>
+			<a href="#0" class="nav__link">About</a>
 		</li>
 
 		<li class="nav__list-item">
-			<a href="#" class="nav__link">Contact</a>
+			<a href="#0" class="nav__link">Contact</a>
 		</li>
 	</ul>
 </nav>
