b300124dff08fa33f81740c257babd7a00804aee
Sergej Smelov <sergeysmelow@gmail.com>
2023-04-15 20:45:06 +0500

    Style the counter

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,16 +1,74 @@
 <script>
-	import iconMinus from "./images/icon-minus.svg"
-	import iconPlus from "./images/icon-plus.svg"
 </script>
 
 <div class="counter">
 	<button class="button-icon">
-		<img src={iconMinus} alt="remove" class="counter__icon" />
+		<span class="sr-only">Add</span>
+		<svg
+			class="counter__icon"
+			width="12"
+			height="4"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:xlink="http://www.w3.org/1999/xlink"
+			><defs
+				><path
+					d="M11.357 3.332A.641.641 0 0 0 12 2.69V.643A.641.641 0 0 0 11.357 0H.643A.641.641 0 0 0 0 .643v2.046c0 .357.287.643.643.643h10.714Z"
+					id="a" /></defs
+			><use fill="#FF7E1B" fill-rule="nonzero" xlink:href="#a" /></svg>
 	</button>
 
 	<p class="value">0</p>
 
 	<button class="button-icon">
-		<img src={iconPlus} alt="add" class="counter__icon" />
+		<span class="sr-only">remove</span>
+		<svg
+			class="counter__icon"
+			width="12"
+			height="12"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:xlink="http://www.w3.org/1999/xlink"
+			><defs
+				><path
+					d="M12 7.023V4.977a.641.641 0 0 0-.643-.643h-3.69V.643A.641.641 0 0 0 7.022 0H4.977a.641.641 0 0 0-.643.643v3.69H.643A.641.641 0 0 0 0 4.978v2.046c0 .356.287.643.643.643h3.69v3.691c0 .356.288.643.644.643h2.046a.641.641 0 0 0 .643-.643v-3.69h3.691A.641.641 0 0 0 12 7.022Z"
+					id="b" /></defs
+			><use fill="#FF7E1B" fill-rule="nonzero" xlink:href="#b" /></svg>
 	</button>
 </div>
+
+<style>
+	.counter {
+		height: 3.5rem;
+		border-radius: 0.625rem;
+
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		background-color: var(--color-neutral-200);
+	}
+
+	.value {
+		color: var(--color-neutral-800);
+		font-weight: var(--font-weight-bold);
+	}
+
+	.button-icon {
+		position: relative;
+
+		height: 100%;
+		border-radius: 0.625rem;
+
+		padding-inline: 1.5rem;
+	}
+
+	.counter__icon use {
+		transition: 150ms ease-in-out;
+	}
+
+	/* Active states in the design are barely visible.
+	It would have been better to add a shade to the buttons themselves */
+
+	.counter__icon:hover use {
+		fill: hsl(26 100% 55% / 0.75);
+	}
+</style>
