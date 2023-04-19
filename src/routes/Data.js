2a490c8c4743ed80405335ce6b3fae5062d1fe04
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-04-19 14:15:30 +0500

    Extract data into a js file (needs more work)

---
+++ src/routes/Data.js
@@ -0,0 +1,19 @@
+import image1 from "../lib/images/image-product-1.jpg"
+import image2 from "../lib/images/image-product-2.jpg"
+import image3 from "../lib/images/image-product-3.jpg"
+import image4 from "../lib/images/image-product-4.jpg"
+import image1thumbnail from "../lib/images/image-product-1-thumbnail.jpg"
+import image2thumbnail from "../lib/images/image-product-2-thumbnail.jpg"
+import image3thumbnail from "../lib/images/image-product-3-thumbnail.jpg"
+import image4thumbnail from "../lib/images/image-product-4-thumbnail.jpg"
+
+export const product = {
+	name: "Fall Limited Edition Sneakers",
+	images: [image1, image2, image3, image4],
+	thumbnails: [
+		image1thumbnail,
+		image2thumbnail,
+		image3thumbnail,
+		image4thumbnail,
+	],
+}
