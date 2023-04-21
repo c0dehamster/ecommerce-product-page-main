289a42ff9f0c42275a50ecbcc36a196d82571aa9
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2023-04-21 21:28:50 +0500

    Rewrite the thumbnail as radio button

---
+++ src/lib/ThumbnailRadio.svelte
@@ -0,0 +1,57 @@
+<script>
+	export let id
+	export let thumbnailImage
+	export let group
+</script>
+
+<label for={id} class="thumbnail">
+	<img src={thumbnailImage} alt="" class="thumbnail__image" />
+	<input {id} value={id} type="radio" class="thumbnail__radio" bind:group />
+</label>
+
+<style>
+	.thumbnail {
+		position: relative;
+		aspect-ratio: 1;
+		border: none;
+		border-radius: 0.5rem;
+	}
+
+	.thumbnail::before,
+	.thumbnail::after {
+		content: "";
+		position: absolute;
+		inset: 0;
+		border-radius: 0.5rem;
+	}
+
+	.thumbnail::before {
+		z-index: -1;
+
+		background-color: var(--color-primary-400);
+		transition: scale 100ms linear;
+	}
+
+	.thumbnail::after {
+		transition: background-color 100ms ease-in;
+	}
+
+	.thumbnail__image {
+		border-radius: 0.5rem;
+	}
+
+	/* Active states */
+
+	.thumbnail:focus::before {
+		scale: 1.05;
+	}
+
+	.thumbnail:hover::after,
+	.thumbnail:focus::after {
+		background-color: rgb(255 255 255 / 0.25);
+	}
+
+	.thumbnail:checked::after {
+		background-color: rgb(255 255 255 / 0.5);
+	}
+</style>
